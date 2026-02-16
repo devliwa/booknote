47ca7594573569b78b858dcaac57499f2032f522
Charles <Charles.liwananda@gmail.com>
2026-02-16 00:33:30 -0700

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer>
-      <p>Copyright ⓒ {year} | All rights reserved to Charles Liwa</p>
+    <footer className="footer-responsive">
+      <div className="footer-content">
+        <span className="footer-logo" aria-label="Booknote logo" role="img">📚</span>
+        <span className="footer-text">Copyright ⓒ {year} &nbsp;|&nbsp; Charles Liwa</span>
+      </div>
     </footer>
   );
 }
